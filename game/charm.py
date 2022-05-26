cfc4dc74a9b4f3a8a80553bb7072d969e0bb83ac
Thomas Faulhaber <thomasihs365@gmail.com>
2022-05-26 07:53:08 -0700

    Fixed an issue with collision detection on the charms.

--- game/charm.py
+++ game/charm.py
@@ -4,16 +4,20 @@ import arcade
 from stage import Stage
 from utils import distance
 
-class Charm(arcade.SpriteSolidColor):
+
+class Charm(arcade.SpriteCircle):
     SPEED = 350
     ACCELERATION = 100
+    RADIUS = 30
+    COLOR = arcade.csscolor.GREENYELLOW
 
     def __init__(self, x: float, y: float, stage: Stage):
-        super().__init__(35, 35, arcade.csscolor.GREENYELLOW)
+        super().__init__(Charm.RADIUS, Charm.COLOR)
         self.set_position(x, y)
         self.speed = Charm.SPEED
         self.stage = stage
         self.stage.charms.append(self)
+        self.rect_width = math.sqrt(math.pow(Charm.RADIUS, 2) * 2)
 
     def on_update(self, delta_time: float):
         x, y = self.position
@@ -30,3 +34,12 @@ class Charm(arcade.SpriteSolidColor):
             self.remove_from_sprite_lists()
 
         self.speed += Charm.ACCELERATION * delta_time
+
+    def draw(self):
+        arcade.draw_rectangle_filled(
+            self.position[0],
+            self.position[1],
+            self.rect_width,
+            self.rect_width,
+            Charm.COLOR,
+        )
--- game/utils.py
+++ game/utils.py
@@ -1,4 +1,5 @@
 import math
 
+
 def distance(x1: float, y1: float, x2: float, y2: float) -> float:
     return math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
